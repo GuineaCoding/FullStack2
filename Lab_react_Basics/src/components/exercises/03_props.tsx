20aa7106701a97de54ba2dfd6efcd2bf89c1a81b
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-05-29 20:33:01 +0100

    Lab 1 - Exercise 3 (Props).

--- Lab_react_Basics/src/components/exercises/03_props.tsx
+++ Lab_react_Basics/src/components/exercises/03_props.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React from 'react';
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
-const Demo: React.FC = () => {
-  return (
-      <h1>TODO</h1>
-  );
+type Module = {
+  name: string;
+  noLectures: number;
+  noPracticals: number;
 };
 
-export default Demo;
+type Props = {
+  course: string;
+  modules: Module[];
+};
+
+const ModuleTable = ({ course, modules }: Props) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          {modules.map((module, index) => (
+            <tr key={index}>
+              <td>{module.name}</td>
+              <td>{module.noLectures}</td>
+              <td>{module.noPracticals}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ModuleTable;
