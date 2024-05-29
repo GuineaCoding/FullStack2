e1e6aa0b1396f43fe6bf2f2e820d02e2747d5c3e
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-05-29 20:11:54 +0100

    Exercise 2 (Embedded variables).

--- Lab_react_Basics/src/components/exercises/02_embeddedVariables.tsx
+++ Lab_react_Basics/src/components/exercises/02_embeddedVariables.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import React from 'react';
 
+const ModuleTable = () => {
+  const modules = [
+    { name: 'DevOps', noLectures: 2, noPracticals: 2 },
+    { name: 'Enterprise Web Dev', noLectures: 3, noPracticals: 2 },
+  ];
 
-const Demo: React.FC = () => {
   return (
-      <h1>TODO</h1>
+    <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody>
+        {modules.map((module, index) => (
+          <tr key={index}>
+            <td>{module.name}</td>
+            <td>{module.noLectures}</td>
+            <td>{module.noPracticals}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
-};
+}
 
-export default Demo;
+export default ModuleTable;
---
+++ Lab_react_Basics/src/components/exercises/02_embeddedVars.tsx
@@ -0,0 +1,5 @@
+const modules = [
+    { name: 'DevOps', noLectures: 2, noPracticals: 2 },
+    { name: 'Enterprise Web Dev', noLectures: 3, noPracticals: 2 },
+  ];
+  
